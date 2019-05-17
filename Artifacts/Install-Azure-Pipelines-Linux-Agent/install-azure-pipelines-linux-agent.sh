013a0cb277187abe3fe7f811cd0059fb3b26a1b3
Dylan Smith <optikal@shaw.ca>
2019-05-17 17:48:39 +0000

    added AGENT_ALLOW_RUNASROOT

--- Artifacts/Install-Azure-Pipelines-Linux-Agent/install-azure-pipelines-linux-agent.sh
+++ Artifacts/Install-Azure-Pipelines-Linux-Agent/install-azure-pipelines-linux-agent.sh
@@ -14,6 +14,7 @@ mkdir agent
 cd agent
 wget -O pipelines-agent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.150.3/vsts-agent-linux-x64-2.150.3.tar.gz
 tar zxvf pipelines-agent.tar.gz
+export AGENT_ALLOW_RUNASROOT=true
 ./config.sh --unattended --url $1 --auth pat --token $2 --pool $3 --acceptTeeEula
 sudo ./svc.sh install
 sudo ./svc.sh start
