5ee32b2c9f1aff4d9003cb585a5ac8c868bcc07a
Dylan Smith <optikal@shaw.ca>
2019-05-17 17:31:26 +0000

    Update install-azure-pipelines-linux-agent.sh

--- Artifacts/Install-Azure-Pipelines-Linux-Agent/install-azure-pipelines-linux-agent.sh
+++ Artifacts/Install-Azure-Pipelines-Linux-Agent/install-azure-pipelines-linux-agent.sh
@@ -14,6 +14,6 @@ mkdir agent
 cd agent
 wget -O pipelines-agent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.150.3/vsts-agent-linux-x64-2.150.3.tar.gz
 tar zxvf pipelines-agent.tar.gz
-sudo ./config.sh --unattended --url $1 --auth pat --token $2 --pool $3 --acceptTeeEula
+./config.sh --unattended --url $1 --auth pat --token $2 --pool $3 --acceptTeeEula
 sudo ./svc.sh install
 sudo ./svc.sh start
