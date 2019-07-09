78b6c48129a232fc57cc6b7ac783db21cc176015
Dylan Smith <optikal@shaw.ca>
2019-07-09 16:15:38 -0500

    updated agent version to 2.154.1

--- Artifacts/Install-Azure-Pipelines-Linux-Agent/install-azure-pipelines-linux-agent.sh
+++ Artifacts/Install-Azure-Pipelines-Linux-Agent/install-azure-pipelines-linux-agent.sh
@@ -12,7 +12,7 @@
 cd /usr/bin
 mkdir agent
 cd agent
-wget -O pipelines-agent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.150.3/vsts-agent-linux-x64-2.150.3.tar.gz
+wget -O pipelines-agent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.154.1/vsts-agent-linux-x64-2.154.1.tar.gz
 tar zxvf pipelines-agent.tar.gz
 export AGENT_ALLOW_RUNASROOT=true
 ./config.sh --unattended --url $1 --auth pat --token $2 --pool $3 --acceptTeeEula
