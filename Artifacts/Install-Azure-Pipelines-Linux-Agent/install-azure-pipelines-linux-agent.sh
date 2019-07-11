2a6327899607d01d1a1feeee32d60f4bb7b52e85
Dylan Smith <optikal@shaw.ca>
2019-07-11 13:32:52 -0500

    Added a param to set agent name - and --once

--- Artifacts/Install-Azure-Pipelines-Linux-Agent/install-azure-pipelines-linux-agent.sh
+++ Artifacts/Install-Azure-Pipelines-Linux-Agent/install-azure-pipelines-linux-agent.sh
@@ -6,6 +6,7 @@
 # $1    URL
 # $2    Personal Access Token
 # $3    Pool Name
+# $4    Agent Name
 #
 ###
 
@@ -15,6 +16,6 @@ cd agent
 wget -O pipelines-agent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.154.1/vsts-agent-linux-x64-2.154.1.tar.gz
 tar zxvf pipelines-agent.tar.gz
 export AGENT_ALLOW_RUNASROOT=true
-./config.sh --unattended --url $1 --auth pat --token $2 --pool $3 --acceptTeeEula
+./config.sh --unattended --url $1 --auth pat --token $2 --pool $3 --agent $4 --once --acceptTeeEula
 sudo ./svc.sh install
 sudo ./svc.sh start
